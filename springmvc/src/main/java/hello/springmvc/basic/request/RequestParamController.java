de12c7970b406f52f99bff8da09c53dedc47368f
hanjaegyeong <hjg000223@naver.com>
2023-01-04 22:42:43 +0900

    feat: HTTP요청 파라미터_@RequestParam

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -6,6 +6,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 
@@ -21,5 +23,34 @@ public class RequestParamController {
         int age = Integer.parseInt(request.getParameter("age"));
         log.info("username={}, age={}", username, age);
         response.getWriter().write("ok");
+    }
+
+    @ResponseBody //RequstMapping으로 인해 String 반환시 ok.jsp라는 view 연결할까봐 @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String mamberName,
+            @RequestParam("age") int age) {
+
+        log.info("username={}, age={}", mamberName, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String userName, //받을 파라미터와 이름 같으면 생략 가능
+            @RequestParam int age) {
+
+        log.info("username={}, age={}", userName, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(
+            @RequestParam(required = true, defaultValue = "guest") String userName,
+             int age) { //required, defaltValue 지정 가능
+        log.info("username={}, age={}", userName, age);
+        return "ok";
     }
 }
