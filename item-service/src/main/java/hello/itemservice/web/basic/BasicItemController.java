07cdf948d6ec0fbc17db674d5b65b2a760451f96
hanjaegyeong <hjg000223@naver.com>
2023-01-06 02:42:40 +0900

    feat: 상품판매_상품 등록 기능 구현

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,16 +17,10 @@ import java.util.List;
 public class BasicItemController {
     private final ItemRepository itemRepository;
 
-//    @RequiredArgsConstructor가 아래 생성자 역할
-//    @Autowired
-//    public BasicItemController(ItemRepository itemRepository) {
-//        this.itemRepository = itemRepository;
-//    }
-
     @GetMapping
     public String items(Model model) { //전체 상품 목록
         List<Item> items = itemRepository.findAll();
-        model.addAttribute("items", items);
+        model.addAttribute("items", items); //모델에 attribute 담으면 view에서 지정한 이름으로 변수 사용 가능!
         return "basic/items";
     }
 
@@ -40,22 +31,50 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @GetMapping("/add") //상품 등록
+    @GetMapping("/add") //상품 등록 GET요청(URI로 view접속시)
     public String addForm() {
         return "basic/addForm";
     }
 
-    @PostMapping("/add") //상품 등록. 동일 url이지만 Post임! 버튼으로(Form) 응답 오는 경우
-    public String save() {
-        return "basic/addForm";
+    //    @PostMapping("/add")//상품 등록. 버튼으로(Form) 응답 오는 경우
+    public String addItemV1(@RequestParam String itemName,
+                            @RequestParam int price,
+                            @RequestParam Integer quantity,
+                            Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
-    /**
-     * 테스트용 데이터 추가
-     */
+    // 더 쉬운 버전
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) { //@ModelAttribute사용해 위 코드와 동일한 역할
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //@ModelAttribute가 item을 모델에 자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+//    @PostMapping("/add")    //"item" 모델name 지정 안해주면 자동으로 받는 어트리뷰트 첫글자 소문자화해서 name지정
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {    //애노테이션 생략해도 Sring 제외 타입은 모두 Model로 받음
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    //테스트용 데이터
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("testA", 10000, 10));
         itemRepository.save(new Item("testB", 20000, 20));
     }
+
 }
