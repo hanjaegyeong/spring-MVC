724076b46f418bf47b3487f354366e7f6be796f6
hanjaegyeong <hjg000223@naver.com>
2023-01-12 17:00:53 +0900

    feat: 상품 수정 로직 추가

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -70,11 +70,26 @@ public class BasicItemController {
         return "basic/item";
     }
 
+    // 상품 수정 폼 GET
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    // 상품 수정 처리 POST
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        //컨트롤러에 매핑된 path 변수를 리다이렉트에서도 사용 가능
+        return "redirect:/basic/items/{itemId}"; // 리다이렉트!!*** 상품 상세 화면으로 이동
+    }
+
     //테스트용 데이터
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("testA", 10000, 10));
         itemRepository.save(new Item("testB", 20000, 20));
     }
-
 }
