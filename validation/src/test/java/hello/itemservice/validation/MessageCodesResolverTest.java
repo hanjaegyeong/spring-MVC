0e78b7d5edb1902a052e492a2d757cb747c389d6
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-08-19 00:59:56 +0900

    feat: 오류 코드와 메시지 처리_v3+v4 rejectValue()의 MessageCodesResolver

---
+++ validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,31 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.ObjectError;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+//MessageCodesResolver: rejectValue() , reject()가 내부에서 사용하는 MessageCodesResolver 객체는 필드값, 객체명, 필드타입, 오류코드 등을 저장하여 자세한 메시지부터 차례대로 찾아줌
+public class MessageCodesResolverTest {
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        Assertions.assertThat(messageCodes).containsExactly("required.item", "required"); //가장 자세, 덜 자세 두 값 다 가짐!
+    }
+
+    @Test
+    void messageCodesResolverFiled() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName", //가장 자세
+                "required.itemName", //덜 자세
+                "required.java.lang.String", //타입
+                "required" //에러코드 자체
+        );
+    }
+}
