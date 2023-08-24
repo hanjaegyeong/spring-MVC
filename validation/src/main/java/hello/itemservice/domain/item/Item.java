6a4608327e6435a2cf8dbed0eb98f59d5b00cc12
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-08-24 23:17:38 +0900

    feat: Bean Validation 수정/등록 요구사항 미분리 시 문제

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -6,20 +6,22 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+//수정 요구사항에 맞게 변경 -> 등록 요구사항은 무시됨 : 수정/등록 요구사항 구분해서 적용 필요
 @Data
 public class Item {
 
+    @NotNull
     private Long id;
 
     @NotBlank
     private String itemName;
 
-    @NotNull(message = "null값 x")
+    @NotNull
     @Range(min = 1000, max = 1000000)
     private Integer price;
 
     @NotNull
-    @Max(9999)
+//    @Max(9999)
     private Integer quantity;
 
     public Item() {
