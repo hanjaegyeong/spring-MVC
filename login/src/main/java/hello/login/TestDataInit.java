eb186cdb25aba6fb3a6c239a0b6959ede4f38af9
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-10-08 11:32:08 +0900

    feat: 테스트데이터 오타 수정

--- login/src/main/java/hello/login/TestDataInit.java
+++ login/src/main/java/hello/login/TestDataInit.java
@@ -26,7 +26,7 @@ public class TestDataInit {
 
         Member member = new Member();
         member.setLoginId("test");
-        member.setPassword("teset!");
+        member.setPassword("test!");
         member.setName("테스터");
 
         memberRepository.save(member);
