efd576a04f7faf0039b58c40bb098bcd3546de42
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-17 23:53:53 +0900

    feat: 필터 도입부 주석 추가

--- login/src/main/java/hello/login/web/item/ItemController.java
+++ login/src/main/java/hello/login/web/item/ItemController.java
@@ -25,6 +25,7 @@ public class ItemController {
 
     @GetMapping
     public String items(Model model) {
+        //로그인 여부 체크 -> 모든 컨트롤러마다 체크 넣기 힘겨움
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "items/items";
