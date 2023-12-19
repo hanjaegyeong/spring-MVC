642e91080902e17189f30a2a3b60fa1ef5c839b4
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-19 12:35:11 +0900

    feat: 서블릿 필터-인증 체크

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,19 @@ public class WebConfig {
     @Bean
     public FilterRegistrationBean logFilter() { //FilterRegistrationBean: 필터 빈
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setFilter(new LogFilter()); // 필터 로그 남기는 LogFilter 객체를 빈으로 등록
         filterRegistrationBean.setOrder(1); // 필터 체인 순서
         filterRegistrationBean.addUrlPatterns("/*"); // 어떤 url에 적용할지 /* : 모든 url
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() { //로그인 체크 필터 빈 등록
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2); // 필터 체인 순서 2번에 등록
+        filterRegistrationBean.addUrlPatterns("/*");
 
         return filterRegistrationBean;
     }
--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -72,7 +73,7 @@ public class LoginController {
     }
 
     // 스프링 HttpSession 이용
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -90,6 +91,27 @@ public class LoginController {
         //세션에 로그인 회원 정보 보관
         session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다"); //reject: global error(검증 단원에서 배움)
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+        //세션이 있으면 해당 세션 반환, 없으면 신규 세션 생성
+        HttpSession session = request.getSession();
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+        return "redirect:" + redirectURL; // url에 redirectURL 있으면 해당 경로로 이동
     }
 
     //쿠키만 이용
