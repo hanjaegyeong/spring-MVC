d9e5d82588cc536decd5274a933921edf4eab75b
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-20 15:23:38 +0900

    feat: 서블릿 예외처리 - 인터셉터 재호출 문제 해결(화이트리스트)

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -1,15 +1,27 @@
 package hello.exception;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/css/**", "/*.ico", "/error", "/error-page/**" //오류 페이지 경로
+                );
+    }
 
     @Bean
     public FilterRegistrationBean logFilter() {
