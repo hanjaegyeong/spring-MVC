87df4d145cfbac18ce929364c56889b6ca41104e
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-20 21:51:16 +0900

    feat: @ExceptionHandler

---
+++ exception/src/main/java/hello/exception/exhandler/ErrorResult.java
@@ -0,0 +1,9 @@
+package hello.exception.exhandler;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
