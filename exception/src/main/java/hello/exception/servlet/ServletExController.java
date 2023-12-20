7009b3ec1715c9177a7983305c97972963c6a346
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-20 14:34:23 +0900

    feat: 서블릿 예외처리 - 오류 화면 제공

--- exception/src/main/java/hello/exception/servlet/ServletExController.java
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -18,6 +18,7 @@ public class ServletExController {
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404, "404 오류!");
     }
+
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500);
