fbe3f96cef2c0612868edbf81f6bdeac8e979c66
hanjaegyeong <87923699+hanjaegyeong@users.noreply.github.com>
2023-12-23 04:46:41 +0900

    feat: 스프링에 컨버터 적용

--- typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,13 @@ public class HelloController {
     @GetMapping("/hello-v2")
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam("ipPort") IpPort ipPort) { // 부트 3.2 버전부턴 RequestParam에 name속성 기입 필요
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
         return "ok";
     }
 }
